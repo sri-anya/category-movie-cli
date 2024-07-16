762f486629445b5452d10ed48f32eb69484cac87
Aastha <aasthasaxena100@gmail.com>
2024-07-16 16:18:30 -0400

    started category model

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,14 @@
 
 from models.__init__ import CONN, CURSOR
 import ipdb
+from models.category import Category
+
+# def reset_database():
+#     Category.drop_table()
+#     # Department.drop_table()
+#     Category.create_table()
+#     # Employee.create_table()
 
 
+# reset_database()
 ipdb.set_trace()
